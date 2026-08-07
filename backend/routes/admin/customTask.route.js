5a8c3dce0c5b5db77ec1320a785d4d46124085e4
guddugiri1 <guddugiri216@gmail.com>
2026-08-08 03:00:56 +0530

    fix(api): remove duplicate verifyAdminAuthToken from customTask.route.js so admin fetch API works correctly

--- backend/routes/admin/customTask.route.js
+++ backend/routes/admin/customTask.route.js
@@ -2,15 +2,14 @@ const express = require("express");
 const route = express.Router();
 
 const checkAccessWithSecretKey = require("../../checkAccess");
-const verifyAdminAuthToken = require("../../middleware/verifyAdminAuthToken.middleware");
 const customTaskController = require("../../controllers/admin/customTask.controller");
 
-route.post("/create", verifyAdminAuthToken, checkAccessWithSecretKey(), customTaskController.createTask);
-route.get("/fetch", verifyAdminAuthToken, checkAccessWithSecretKey(), customTaskController.fetchTasks);
-route.patch("/update", verifyAdminAuthToken, checkAccessWithSecretKey(), customTaskController.updateTask);
-route.delete("/delete", verifyAdminAuthToken, checkAccessWithSecretKey(), customTaskController.deleteTask);
+route.post("/create", checkAccessWithSecretKey(), customTaskController.createTask);
+route.get("/fetch", checkAccessWithSecretKey(), customTaskController.fetchTasks);
+route.patch("/update", checkAccessWithSecretKey(), customTaskController.updateTask);
+route.delete("/delete", checkAccessWithSecretKey(), customTaskController.deleteTask);
 
-route.get("/submissions", verifyAdminAuthToken, checkAccessWithSecretKey(), customTaskController.fetchSubmissions);
-route.post("/verifySubmission", verifyAdminAuthToken, checkAccessWithSecretKey(), customTaskController.verifySubmission);
+route.get("/submissions", checkAccessWithSecretKey(), customTaskController.fetchSubmissions);
+route.post("/verifySubmission", checkAccessWithSecretKey(), customTaskController.verifySubmission);
 
 module.exports = route;
