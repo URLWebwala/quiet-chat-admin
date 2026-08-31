248442a8d99b3774a2dd7c2ef19da4a6410803e4
guddugiri1 <guddugiri216@gmail.com>
2026-08-31 18:38:21 +0530

    fix: restore opening Head tag in _document.tsx and remove app router favicon conflict

--- admin/src/pages/_document.tsx
+++ admin/src/pages/_document.tsx
@@ -1,10 +1,10 @@
-"use client";
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
         <link rel="icon" href="/favicon.ico?v=3" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=3" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=3" />
