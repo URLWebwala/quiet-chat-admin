bab620f7f5081dc8fb3f42742a8fb9f71f9d8c3a
guddugiri1 <guddugiri216@gmail.com>
2026-09-18 16:14:03 +0530

    fix(user): display Android for empty deviceType when not iOS

--- admin/src/pages/User/User.tsx
+++ admin/src/pages/User/User.tsx
@@ -540,7 +540,7 @@ const User = (props: any) => {
               <i className="ri-global-line" style={{ fontSize: "13px" }}></i> Web
             </span>
           );
-        } else if (device === "android") {
+        } else if (device === "android" || device === "") {
           return (
             <span
               style={{
